ab20246f8224b4bf7e64cac3ae0be7c62c002797
Yawn0 <77552501+Yawn0@users.noreply.github.com>
2025-06-15 00:19:03 +0200

    actually linking the database to the UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,26 @@
+import Image from "next/image";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic"; // This is to ensure the page is not cached
 
-const paths = [
-  "/media/20160812_115538.jpg",
-  "/media/20160812_131542.jpg",
-  "/media/20160812_132909.jpg",
-];
-
-const images = paths.map((path, index) => ({
-  id: index + 1,
-  path
-}));
-
 export default async function HomePage() {
 
+  const images = await db.query.images.findMany({
+    orderBy:(model, {desc}) => desc(model.id),
+  });
 
-
-  const posts = await db.query.posts.findMany();
-
-  console.log("posts", posts);
+  console.log("images", images);
 
   return (
     <main className="">
       <div className="flex flex-wrap gap-5">
-        {posts.map(post => (
-          <div key={post.id}>{post.name}</div>
-        ))}
-        {[...images,...images,...images].map((image, index) => (
-          <div key={image.id + index} className="w-48">
-            <img src={image.path} alt="asd" />
+        {[...images].map((image, index) => (
+          <div key={image.id + index} className="flex w-48 flex-col">
+            <img src={image.url} alt={image.name} />
+            <div>{image.name}</div>
           </div>
         ))}
       </div>
-      hello world
     </main>
   );
 }
